1b6866d7036bd1e3328bab3eeffc8b719bf27c92
vladimirmartinez-ef <vemf.vemf@hotmail.com>
2022-03-12 16:35:32 -0600

    mi primer comit

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,76 @@
+<template>
+    <q-layout view="hhh LpR fFf" container style="height: 655px" class="shadow-2 rounded-borders">
+      <q-header reveal class="bg-black glossy">
+        <q-toolbar>
+          <q-btn flat @click="drawerLeft = !drawerLeft" round dense icon="menu" />
+          <q-toolbar-title>Header</q-toolbar-title>
+        </q-toolbar>
+      </q-header>
+
+      <q-footer elevated>
+        <q-toolbar class="glossy">
+          <q-toolbar-title>Footer</q-toolbar-title>
+        </q-toolbar>
+      </q-footer>
+
+      <q-drawer
+        side="left"
+        v-model="drawerLeft"
+        bordered
+        :width="200"
+        :breakpoint="500"
+        class="bg-grey-3"
+      >
+        <q-scroll-area style="height: calc(100% - 150px); margin-top: 150px; border-right: 1px solid #ddd">
+          <q-list padding>
+            <q-item to="/inbox" clickable v-ripple>
+              <q-item-section avatar>
+                <q-icon name="inbox" />
+              </q-item-section>
+
+              <q-item-section>
+                Inbox
+              </q-item-section>
+            </q-item>
+
+            <q-item to="/" active clickable v-ripple>
+              <q-item-section avatar>
+                <q-icon name="star" />
+              </q-item-section>
+
+              <q-item-section>
+                Star
+              </q-item-section>
+            </q-item>
+          </q-list>
+        </q-scroll-area>
+
+        <q-img class="absolute-top" src="https://cdn.quasar.dev/img/material.png" style="height: 150px">
+          <div class="absolute-bottom bg-transparent">
+            <q-avatar size="56px" class="q-mb-sm">
+              <img src="https://cdn.quasar.dev/img/boy-avatar.png">
+            </q-avatar>
+            <div class="text-weight-bold">Razvan Stoenescu</div>
+            <div>@rstoenescu</div>
+          </div>
+        </q-img>
+      </q-drawer>
+
+      <q-page-container>
+        <router-view />
+      </q-page-container>
+    </q-layout>
+</template>
+
+<script>
+import { ref } from 'vue'
+
+export default {
+  setup () {
+    return {
+      drawerLeft: ref(false),
+      drawerRight: ref(true)
+    }
+  }
+}
+</script>
